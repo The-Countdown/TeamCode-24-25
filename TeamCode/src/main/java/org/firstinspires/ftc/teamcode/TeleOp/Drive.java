96e929a08ee1f65972272835b65fc502a0cbb676
coolperson12345678 <161359184+coolperson12345678@users.noreply.github.com>
2024-10-08 01:35:22 -0700

    Update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Drive.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import com.acmerobotics.roadrunner.Pose2d;
@@ -17,9 +18,11 @@ import org.firstinspires.ftc.teamcode.Auto.RoadRunner.MecanumDrive;
 public class Drive extends LinearOpMode {
 
     public static double intakePosUp = 0.42;
-    public static double intakePosDown = 0.48;
+    public static double intakePosDown = 0.52;
 
-    public static double intakeYawMulti = 0.1;
+    public static double intakeYawMulti = 0.001;
+
+    public static double intakeYawCenter = 0.5;; //TODO: Find
 
     public static double intakeRollerSpeed = 1;
 
@@ -36,18 +39,19 @@ public class Drive extends LinearOpMode {
     public static double intakeYawThreshold = 0.1;
 
     //TODO: Tune with driver
-    public static double yStickLMulti = -0.6;
-    public static double xStickLMulti = -0.75;
+    public static double yStickLMulti = 0.6;
+    public static double xStickLMulti = 0.75;
     public static double xStickRMulti = 0.85;
 
     @Override
     public void runOpMode() {
 
         //All motors are goBlida Yellow Jacket 5203 with 435rpm
-        //All servos except for claw and clawAngle are goBilda Dual Mode
-        //Roller is goBilda Speed, both claw are rev, and the rest are goBilda Torque
+        //Roller is goBilda Speed, both claw are rev, and the rest are goBilda Torque servos
 
         DcMotorEx intakeSlide = hardwareMap.get(DcMotorEx.class, "intakeSlide");
+            intakeSlide.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+            intakeSlide.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
         DcMotorEx depositSlide = hardwareMap.get(DcMotorEx.class, "depositSlide");
 
         DcMotorEx leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
@@ -60,14 +64,11 @@ public class Drive extends LinearOpMode {
         CRServo intakeRoller = hardwareMap.get(CRServo.class,"intakeRoller");
 
         Servo depositServo = hardwareMap.get(Servo.class,"depositServo");
-        Servo clawAngle = hardwareMap.get(Servo.class,"clawAngle"); //TODO: Reprogram to default
-        Servo claw = hardwareMap.get(Servo.class,"claw"); //TODO: Reprogram to default
+        Servo clawAngle = hardwareMap.get(Servo.class,"clawAngle");
+        Servo claw = hardwareMap.get(Servo.class,"claw");
 
         Pose2d beginPose = new Pose2d(0, 0, 0); //TODO: Figure out what pos to start with (Changes depending on situation)
         MecanumDrive drive = new MecanumDrive(hardwareMap,beginPose);
-
-        double intakeYawStartPos = intakeYaw.getPosition();
-        double intakePitchStartPos = intakePitch.getPosition(); //TODO: Incorporate
 
         waitForStart();
 
@@ -80,8 +81,8 @@ public class Drive extends LinearOpMode {
             double rotAngle = drive.pose.heading.real;
 
             double xStickR = xStickRMulti * gamepad1.right_stick_x;
-            double xStickL = xStickLMulti * gamepad1.left_stick_x;
-            double yStickL = yStickLMulti * gamepad1.left_stick_y;
+            double xStickL = -xStickLMulti * gamepad1.left_stick_x;
+            double yStickL = -yStickLMulti * gamepad1.left_stick_y; //TODO: Make negative after testing
 
             leftFront.setPower(xStickR + xStickL + yStickL); //TODO: Fix low-power motor
             leftBack.setPower(xStickR - xStickL + yStickL);
@@ -96,13 +97,13 @@ public class Drive extends LinearOpMode {
             depositServo.setPosition(clawUpPos); //TODO: Bind to drop-off sequence
 
             if (Math.abs(gamepad2.right_stick_x) > intakeYawThreshold)
-                intakeYaw.setPosition(intakeYawStartPos + (gamepad2.right_stick_x * intakeYawMulti)); //TODO: Multiply to reduce how much the intake turns
+                intakeYaw.setPosition(intakeYaw.getPosition() + (gamepad2.right_stick_x * intakeYawMulti)); //TODO: Multiply to reduce how much the intake turns
 
             if (Math.abs(gamepad2.right_stick_y) > intakePitchThreshold) {
                 if (gamepad2.right_stick_y < -intakePitchThreshold)
-                    intakePitch.setPosition(intakePosUp);
+                    intakePitch.setPosition(intakePosUp); //TODO: Flip after testing
                 else if (gamepad2.right_stick_y > intakePitchThreshold)
-                    intakePitch.setPosition(intakePosDown);
+                    intakePitch.setPosition(intakePosDown); //TODO: Flip after testing
             }
 
             if (gamepad2.right_bumper)
@@ -123,6 +124,14 @@ public class Drive extends LinearOpMode {
             else if (gamepad2.triangle)
                 claw.setPosition(clawClosed);
 
+            if (gamepad2.dpad_up)
+                intakeYaw.setPosition(intakeYawCenter);
+                intakePitch.setPosition(intakePosUp);
+                intakeSlide.setTargetPosition(-1700);
+                intakeSlide.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+                intakeSlide.setVelocity(850);
+            if (intakeSlide.getCurrentPosition() >= intakeSlide.getTargetPosition() + 50) {
+                intakePitch.setPosition(intakePosDown);
 
             //TODO: Add limits so when the intake gets to a certain point it goes up and straightens
 
@@ -134,6 +143,7 @@ public class Drive extends LinearOpMode {
             telemetry.addData("Claw",claw.getPosition());
             telemetry.addData("Claw Rotation",clawAngle.getPosition());
             telemetry.addData("Deposit Height",depositSlide.getCurrentPosition());
+            telemetry.addData("Intake Height",intakeSlide.getCurrentPosition());
             telemetry.addData("X",gamepad2.cross);
             telemetry.update();
         }
