8923c41fcc19e67e3d5df2688fd433a394a39eda
coolperson12345678 <161359184+coolperson12345678@users.noreply.github.com>
2024-11-13 17:19:02 -0800

    gear slip pos changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -12,16 +12,16 @@ public class Intake extends Robot.HardwareDevices {
     @Config
     public static class IntakePosition {
         // Arm positions
-        public static double armUpL = 0.53;
-        public static double armUpR = 0.47;
-        public static double armDownL = 0.51;
-        public static double armDownR = 0.49;
-        public static double armRestL = 0.405;
-        public static double armRestR = 0.595;
-        public static double armTransferL = 0.575;
-        public static double armTransferR = 0.425;
-        public static double armTransfer2L = 0.54;
-        public static double armTransfer2R = 0.46;
+        public static double armUpL = 0.55;
+        public static double armUpR = 0.45;
+        public static double armDownL = 0.53;
+        public static double armDownR = 0.47;
+        public static double armRestL = 0.425;
+        public static double armRestR = 0.575;
+        public static double armTransferL = 0.6;
+        public static double armTransferR = 0.4;
+        public static double armTransfer2L = 0.56;
+        public static double armTransfer2R = 0.44;
 
         // Elbow positions
         public static double elbowUp = 0.32;
